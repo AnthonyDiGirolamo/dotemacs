b8eddceab00b18611ed8f8100089adbedcd86d88
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-08-15 13:52:21 -0700

    added flyspell-correct-ivy, expand-region

---
+++ emacs.d/elpa/flyspell-correct-0.2/flyspell-correct-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-correct" "0.2" "correcting words with flyspell via custom interface" 'nil :url "https://github.com/d12frosted/flyspell-correct")
