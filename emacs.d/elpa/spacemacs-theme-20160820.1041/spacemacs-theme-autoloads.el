9cbae3a39e2928389d74a948f2eecf4342385784
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-09-07 09:58:30 -0700

    add spacemacs-theme and upgrade atom-one-dark-theme

--- emacs.d/elpa/spacemacs-theme-20160820.1041/spacemacs-theme-autoloads.el
+++ emacs.d/elpa/spacemacs-theme-20160820.1041/spacemacs-theme-autoloads.el
@@ -1,10 +1,10 @@
 ;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22122
-;;;;;;  56803 0 0))
+;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22480
+;;;;;;  16309 0 0))
 ;;; Generated autoloads from spacemacs-common.el
 
 (when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -12,7 +12,7 @@
 ;;;***
 
 ;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (22122 56803 855469 0))
+;;;;;;  "spacemacs-theme-pkg.el") (22480 16309 0 0))
 
 ;;;***
 
