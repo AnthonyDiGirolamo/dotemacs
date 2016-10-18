97486216f4408639a7944013c70a77cd7491700b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-10-18 14:28:56 -0700

    add sicp, update smex, rearrange eshell settings
    and use ivy instead of company in eshell

--- emacs.d/elpa/smex-20151212.1409/smex-autoloads.el
+++ emacs.d/elpa/smex-20151212.1409/smex-autoloads.el
@@ -1,10 +1,11 @@
 ;;; smex-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "smex" "smex.el" (22128 2136 75244 112000))
-;;; Generated autoloads from smex.el
+;;;### (autoloads nil "smex" "../../../../../.emacs.d/elpa/smex-20151212.1409/smex.el"
+;;;;;;  "3eb8092fcfb3498a96723a6953152fe7")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/smex-20151212.1409/smex.el
 
 (autoload 'smex "smex" "\
 
@@ -20,6 +21,12 @@ Like `smex', but limited to commands that are relevant to the active major mode.
 
 
 \(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/smex-20151212.1409/smex-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/smex-20151212.1409/smex.el")
+;;;;;;  (22534 35458 59395 672000))
 
 ;;;***
 
