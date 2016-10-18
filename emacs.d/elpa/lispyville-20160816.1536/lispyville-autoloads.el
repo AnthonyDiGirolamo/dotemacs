6be71ed4fb55a95ab64857fcbdfe829534869716
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-10-18 11:54:14 -0700

    upgrade packages
    use-package, swiper, counsel, ivy, lispy, ace-link, flyspell-correct-ivy, lispyville

--- emacs.d/elpa/lispyville-20160816.1536/lispyville-autoloads.el
+++ emacs.d/elpa/lispyville-20160816.1536/lispyville-autoloads.el
@@ -3,9 +3,9 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "lispyville" "lispyville.el" (22468 32669 0
-;;;;;;  0))
-;;; Generated autoloads from lispyville.el
+;;;### (autoloads nil "lispyville" "../../../../../.emacs.d/elpa/lispyville-20160816.1536/lispyville.el"
+;;;;;;  "fbffef46599a8924cc4f60148a5a9507")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/lispyville-20160816.1536/lispyville.el
 
 (autoload 'lispyville-mode "lispyville" "\
 A minor mode for integrating evil with lispy.
@@ -17,6 +17,12 @@ Binds keys in lispyville-mode-map according to THEME.
 When THEME is not given, `lispville-key-theme' will be used instead.
 
 \(fn &optional THEME)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/lispyville-20160816.1536/lispyville-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/lispyville-20160816.1536/lispyville.el")
+;;;;;;  (22534 28568 658534 908000))
 
 ;;;***
 
