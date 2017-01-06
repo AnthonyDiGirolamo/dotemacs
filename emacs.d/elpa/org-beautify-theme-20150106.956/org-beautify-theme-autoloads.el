ed2fcb6780d3431803abf1a92c1babda9e1eb12b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-01-06 13:37:54 -0800

    upgrade org-mode

--- emacs.d/elpa/org-beautify-theme-20150106.956/org-beautify-theme-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; org-beautify-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("org-beautify-theme.el") (22022 42546
-;;;;;;  938206 154000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; org-beautify-theme-autoloads.el ends here
--- emacs.d/elpa/org-beautify-theme-20150106.956/org-beautify-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-beautify-theme" "20150106.956" "A sub-theme to make org-mode more beautiful." 'nil :keywords '("org" "theme"))
