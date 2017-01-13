32be0e81e1d6175c635cd21930c07e051913db08
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-01-12 21:16:52 -0800

    pico8 changes

--- emacs.d/pico8/pico8.el
+++ emacs.d/pico8/pico8.el
@@ -17,5 +17,51 @@
   (font-lock-add-keywords nil pico8-highlights)
 )
 
+;; (defun run-pico8 ()
+;;   "run a pico-8 cartridge then revert buffer"
+;;   (interactive)
+;;   (let ((current-file-path (file-name-base (buffer-file-name (current-buffer))))
+;;         (pico8-command     (cond ((eq system-type 'cygwin)
+;;                                   "/home/anthony/pico-8_win32/pico8.exe -windowed 1 -home 'C:\cygwin64\home\anthony\heliopause-pico-8' -run "
+;;                                   ;; "/home/anthony/pico-8_win32/pico8.exe -windowed 1 -home C:/cygwin64/home/anthony/heliopause-pico-8 "
+;;                                   )
+;;                                  ((eq system-type 'windows-nt)
+;;                                   "c:/Users/anthony/pico-8_win32/pico8.exe -windowed 1 -home C:\\Users\\anthony\\heliopause-pico-8 -run "
+;;                                   )
+;;                                  (amd/using-pocketchip
+;;                                   "/usr/lib/pico8/pico8 -run ")
+;;                                  (t
+;;                                   "/home/anthony/apps/pico-8/pico8 -run "))))
+;;     (save-buffer)
+;;     (shell-command (concat pico8-command current-file-path))
+;;     ;; (shell-command pico8-command)
+;;     (revert-buffer nil t)))
+
+(defun pico8-token-count ()
+  "Return token counts"
+  ;; (buffer)
+  ;; (interactive "b")
+  (interactive)
+  ;; (message "%s" (current-buffer))
+  ;; (with-current-buffer (current-buffer)
+  (message "%d" (how-many "local"))
+  ;; )
+  ;; (save-excursion
+  ;;   (let* (
+  ;;          (local-count (how-many (rx-(rx (sequence symbol-start "local" symbol-end))))
+  ;;          )
+  ;;     (message "%d" local-count)
+  ;;     ))
+  (rx-to-string (rx (sequence word-start "local" word-end)))
+  )
+
+
+
+;; (with-current-buffer "heliopause.p8"
+;;   (how-many (rx (sequence symbol-start "function" symbol-end))))
+
+;; (with-current-buffer "heliopause.p8"
+;;   (how-many (rx (char "[{("))))
+
 (provide 'pico8)
 ;;; pico8.el ends here
