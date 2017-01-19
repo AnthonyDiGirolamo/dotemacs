3d147c5869d6903edd0380d91b0f407334d6807b
Anthony DiGirolamo <tonymd@tonymd-macbookpro.roam.corp.google.com>
2017-01-19 10:59:04 -0800

    add paradox menu

--- custom.el
+++ custom.el
@@ -3,23 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#fffefe" "#9d0000" "#006a00" "#0e1b00" "#6845138" "#840086" "#003567" "#494949"])
  '(custom-safe-themes
    (quote
-    ("de0b7245463d92cba3362ec9fe0142f54d2bf929f971a8cdf33c0bf995250bcf" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "4156d0da4d9b715c6f7244be34f2622716fb563d185b6facedca2c0985751334" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "51897d0e185a9d350a124afac8d5e95cda53e737f3b33befc44ab02f2b03dab1" "7485c473334e95534b8aaa238bf6d4b7a12777a6bd3cc14d78b627d806e745c9" "9f3181dc1fabe5d58bbbda8c48ef7ece59b01bed606cfb868dd147e8b36af97c" "962dacd99e5a99801ca7257f25be7be0cebc333ad07be97efd6ff59755e6148f" "32e3693cd7610599c59997fee36a68e7dd34f21db312a13ff8c7e738675b6dfc" "6db9acac88c82f69296751e6c6d808736d6ff251dcb34a1381be86efc14fef54" "d6db7498e2615025c419364764d5e9b09438dfe25b044b44e1f336501acd4f5b" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "107420ec61832543bd769e5d6764bedb41ec3d2f8cd4d53a407f7aa74b493dac" "06dbcfac3705aaaa79e1a3264c6fd44ef0cf86ef5ed67930e4007e63a8c1e8ee" "220d0264c92e4bb0df30b599b8d2a096362c22daf76bd0e9315d438de2233872" "16241cf61a64014901a8dfb661cff9e5d9702f743f4e888c26de8c92267242d6" "eb6f7bd5ee41ad5b689e8e33a5ef22ff24cef1551c133cf94b66bc6edfed7e47")))
+    ("962dacd99e5a99801ca7257f25be7be0cebc333ad07be97efd6ff59755e6148f" "32e3693cd7610599c59997fee36a68e7dd34f21db312a13ff8c7e738675b6dfc" "6db9acac88c82f69296751e6c6d808736d6ff251dcb34a1381be86efc14fef54" "d6db7498e2615025c419364764d5e9b09438dfe25b044b44e1f336501acd4f5b" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "107420ec61832543bd769e5d6764bedb41ec3d2f8cd4d53a407f7aa74b493dac" "06dbcfac3705aaaa79e1a3264c6fd44ef0cf86ef5ed67930e4007e63a8c1e8ee" "220d0264c92e4bb0df30b599b8d2a096362c22daf76bd0e9315d438de2233872" "16241cf61a64014901a8dfb661cff9e5d9702f743f4e888c26de8c92267242d6" "eb6f7bd5ee41ad5b689e8e33a5ef22ff24cef1551c133cf94b66bc6edfed7e47")))
  '(elfeed-feeds (quote ("http://planet.emacsen.org/atom.xml")))
  '(elscreen-display-tab nil)
- '(hl-sexp-background-color "#060404")
  '(inhibit-startup-screen nil)
  '(mac-mouse-wheel-smooth-scroll nil)
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate")))
  '(magit-status-buffer-switch-function (quote switch-to-buffer))
  '(package-selected-packages
    (quote
-    (elfeed sql-indent pylint moonscript projectile s dash smex sicp swiper ivy xterm-color fish-mode vimrc-mode company-shell kanban neotree vlf exec-path-from-shell evil dired-ranger dired-subtree benchmark-init nlinum nlinum-relative lispy edit-server evil-visualstar json-mode yasnippet lispyville expand-region flyspell-correct-ivy flyspell-correct company-lua yari yaml-mode which-key wgrep-pt wgrep-ag web-mode subatomic256-theme solarized-theme shackle scss-mode robe rich-minority relative-line-numbers rainbow-delimiters pt powerline pandoc-mode ox-gfm org-bullets markdown-mode+ key-chord js2-mode jedi ido-vertical-mode ibuffer-vc ibuffer-projectile htmlize haml-mode go-mode fuzzy flymake-yaml flymake-ruby flymake-haml flycheck-package flx-ido fireplace feature-mode f eyebrowse evil-commentary esup emmet-mode edbi discover-my-major discover cyberpunk-theme counsel company-inf-ruby coffee-mode base16-theme atom-one-dark-theme anzu ample-theme ag ace-link)))
+    (paradox elfeed sql-indent pylint moonscript s dash smex sicp swiper ivy xterm-color fish-mode vimrc-mode company-shell kanban neotree vlf exec-path-from-shell evil dired-ranger dired-subtree benchmark-init nlinum nlinum-relative lispy edit-server evil-visualstar json-mode yasnippet lispyville expand-region flyspell-correct-ivy flyspell-correct company-lua yari yaml-mode which-key wgrep-pt wgrep-ag web-mode subatomic256-theme solarized-theme shackle scss-mode robe rich-minority relative-line-numbers rainbow-delimiters pt powerline pandoc-mode ox-gfm org-bullets markdown-mode+ key-chord js2-mode jedi ido-vertical-mode ibuffer-vc ibuffer-projectile htmlize haml-mode go-mode fuzzy flymake-yaml flymake-ruby flymake-haml flycheck-package flx-ido fireplace feature-mode f eyebrowse evil-commentary esup emmet-mode edbi discover-my-major discover cyberpunk-theme counsel company-inf-ruby coffee-mode base16-theme atom-one-dark-theme anzu ample-theme ag ace-link)))
+ '(paradox-github-token t)
  '(powerline-utf-8-separator-left 11136)
  '(powerline-utf-8-separator-right 11138)
  '(projectile-mode-line (quote (:eval (format " p[%s]" (projectile-project-name)))))
