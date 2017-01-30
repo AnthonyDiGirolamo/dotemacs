b5838f334a87f357ff99eb77d753e4e6da32e2fd
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-01-30 12:38:32 -0800

    amd/copy-buffer, better-shell

---
+++ elpa/better-shell-20160830.1451/better-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "better-shell" "20160830.1451" "Better shell management" '((cl-lib "0.5")) :url "https://github.com/killdash9/better-shell" :keywords '("convenience"))
