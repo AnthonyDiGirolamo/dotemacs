be7ef0c46290a29ef010e181773afe35b7524cd1
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-02-09 10:13:11 -0800

    upgrade kanban.el

--- elpa/kanban-20170203.1701/kanban-autoloads.el
+++ elpa/kanban-20170203.1701/kanban-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "kanban" "kanban.el" (22505 26641 463687 133000))
+;;;### (autoloads nil "kanban" "kanban.el" (22684 45245 0 0))
 ;;; Generated autoloads from kanban.el
 
 (autoload 'kanban-headers "kanban" "\
@@ -12,26 +12,32 @@ states. If the table is too narrow, the only the first n TODO
 states will be shown, with n as the number of columns in your
 table.
 
-\(fn COLUMN)" nil nil)
+Only not already present TODO states will be filled into empty
+fields starting from the current column. All columns left of
+the current one are left untouched.
+
+Optionally ignore fields in columns left of STARTCOLUMN
+
+\(fn &optional STARTCOLUMN)" nil nil)
 
 (autoload 'kanban-zero "kanban" "\
 Zero-state Kanban board: This Kanban board just displays all
 org-mode headers which have a TODO state in their respective TODO
 state. Useful for getting a simple overview of your tasks.
 
-Gets the COLUMN and ROW via TBLFM ($# and @#) and can get a string as MATCH to select only entries with a matching tag, as well as a list of org-mode files as the SCOPE to search for tasks.
+Gets the ROW and COLUMN via TBLFM ($# and @#) and can get a string as MATCH to select only entries with a matching tag, as well as a list of org-mode files as the SCOPE to search for tasks.
 
-\(fn COLUMN ROW &optional MATCH SCOPE)" nil nil)
+\(fn ROW COLUMN &optional MATCH SCOPE)" nil nil)
 
 (autoload 'kanban-todo "kanban" "\
-Kanban TODO item grabber. Fills the first row of the kanban
+Kanban TODO item grabber. Fills the first column of the kanban
 table with org-mode TODO entries, if they are not in another cell
 of the table. This allows you to set the state manually and just
 use org-mode to supply new TODO entries.
 
-Gets the COLUMN and all other CELS via TBLFM ($# and @2$2..@>$>) and can get a string as MATCH to select only entries with a matching tag, as well as a list of org-mode files as the SCOPE to search for tasks.
+Gets the ROW and all other CELS via TBLFM ($# and @2$2..@>$>) and can get a string as MATCH to select only entries with a matching tag, as well as a list of org-mode files as the SCOPE to search for tasks.
 
-\(fn COLUMN CELS &optional MATCH SCOPE)" nil nil)
+\(fn ROW CELS &optional MATCH SCOPE)" nil nil)
 
 ;;;***
 
