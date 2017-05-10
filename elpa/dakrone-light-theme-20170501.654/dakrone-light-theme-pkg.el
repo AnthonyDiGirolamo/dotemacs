2f4ffb603deb066e9773a5a588a029c110fe0f37
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-05-10 09:06:14 -0700

    add dakrone-light theme

---
+++ elpa/dakrone-light-theme-20170501.654/dakrone-light-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dakrone-light-theme" "20170501.654" "dakrone's custom light theme" 'nil :commit "4b3f3ba8e2ffc35e537507894620245c96ff8965" :url "https://github.com/dakrone/dakrone-light-theme" :keywords '("color" "themes" "faces" "this" "file" "is" "not" "part" "of" "gnu" "emacs."))
