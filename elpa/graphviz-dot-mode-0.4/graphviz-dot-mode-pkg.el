2693178b0121140ea1e94aaf45cc6c630857fe32
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-05-30 13:25:11 -0700

    add graphviz-dot-mode

---
+++ elpa/graphviz-dot-mode-0.4/graphviz-dot-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "graphviz-dot-mode" "0.4" "Mode for the dot-language used by graphviz (att)." 'nil :commit "7301cc276206b6995d265bcb9eb308bb83c760be" :url "http://ppareit.github.com/graphviz-dot-mode/" :keywords '("mode" "dot" "dot-language" "dotlanguage" "graphviz" "graphs" "att"))
