cef2d99ed642aaa9b6ffb6e59237a6d0c7c8d1cb
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-10-20 09:56:41 -0700

    add helpful package and set bindings

---
+++ elpa/helpful-0.2/helpful-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helpful" "0.2" "a better *help* buffer" '((emacs "24.4") (dash "2.12.0") (s "1.11.0") (elisp-refs "1.2")) :commit "b9a06978b6ffcd7f0ea213a6f534fa39318f0050" :url "https://github.com/Wilfred/helpful" :keywords '("help" "lisp"))
