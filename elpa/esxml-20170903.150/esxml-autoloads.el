7c55e7e7d4377beb88e174d1cd6c9c0c7e47bbc2
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-10-23 18:09:13 -0700

    add nov.el

---
+++ elpa/esxml-20170903.150/esxml-autoloads.el
@@ -0,0 +1,16 @@
+;;; esxml-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("esxml-pkg.el" "esxml-query.el" "esxml.el")
+;;;;;;  (23022 37167 198675 287000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; esxml-autoloads.el ends here
---
+++ elpa/nov-0.2.1/nov-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nov" "0.2.1" "Featureful EPUB reader mode" '((dash "2.12.0") (esxml "0.3.3") (emacs "24.4")) :commit "19ab463864f137b43704b4f34173349c88e84d8e" :url "https://github.com/wasamasa/nov.el" :keywords '("hypermedia" "multimedia" "epub"))
