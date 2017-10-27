0dc1956bc7bb4eaa893a6eb24c33022b255ea8b3
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-10-27 09:11:39 -0700

    fix some nov.el settings and add ace-link support

--- elpa/nov-0.2.1/nov-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "nov" "0.2.1" "Featureful EPUB reader mode" '((dash "2.12.0") (esxml "0.3.3") (emacs "24.4")) :commit "19ab463864f137b43704b4f34173349c88e84d8e" :url "https://github.com/wasamasa/nov.el" :keywords '("hypermedia" "multimedia" "epub"))
