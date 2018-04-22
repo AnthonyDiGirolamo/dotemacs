f6cbea2b61889e5a1de68795c4f20572ef2be1ce
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2018-04-22 13:32:07 -0700

    Install nov-mode

---
+++ elpa/nov-0.2.3/nov-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nov" "0.2.3" "Featureful EPUB reader mode" '((dash "2.12.0") (esxml "0.3.3") (emacs "24.4")) :commit "e7bb37334ca85ce0e3f5c980464f652266a11218" :keywords '("hypermedia" "multimedia" "epub") :url "https://github.com/wasamasa/nov.el")
