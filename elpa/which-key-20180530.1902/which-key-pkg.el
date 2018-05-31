fa517dc6ea67a6ef4495b1c2dd38464bb667c153
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2018-05-31 16:59:28 -0700

    Package updates
    upgrade which-key
    move evil, evil-commentary, evil-matchit, evil-surround to melpa-stable

---
+++ elpa/which-key-20180530.1902/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20180530.1902" "Display available keybindings in popup" '((emacs "24.4")) :commit "2c915407e15cc2f33d40a5efbc063596dc493991" :url "https://github.com/justbur/emacs-which-key")
