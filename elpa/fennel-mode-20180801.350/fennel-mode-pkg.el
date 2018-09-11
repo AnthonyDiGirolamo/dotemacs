b2b0a425b2031bf059e6226dee4a489ee2a91629
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2018-09-11 08:20:23 -0700

    add blimp and fennel-mode

---
+++ elpa/fennel-mode-20180801.350/fennel-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fennel-mode" "20180801.350" "a major-mode for editing Fennel code" 'nil :commit "b1f07dff0b16a0bc912528e3c1a70231488a1399" :keywords '("languages" "tools") :authors '(("Phil Hagelberg")) :maintainer '("Phil Hagelberg") :url "https://gitlab.com/technomancy/fennel-mode")
