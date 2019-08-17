cf7ed26cbcac570884f9b055c0821adb1f37bbd0
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2019-08-16 19:22:17 -0700

    Upgrade and swith back to moe-dark theme, some font size changes

--- elpa/moe-theme-20180617.200/moe-theme-autoloads.el
+++ elpa/moe-theme-20180617.200/moe-theme-autoloads.el
@@ -25,6 +25,8 @@
 ;;;### (autoloads nil "moe-theme" "moe-theme.el" (0 0 0 0))
 ;;; Generated autoloads from moe-theme.el
 
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "moe-theme" '("moe-")))
 
 ;;;***
