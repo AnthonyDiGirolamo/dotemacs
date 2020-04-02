9f136153912abe81649917b25d71bed67cf61397
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-04-01 17:13:48 -0700

    tangle-dotfiles.sh: don't tangle README.org, let emacs init.el do that

--- tangle-dotfiles.sh
+++ tangle-dotfiles.sh
@@ -3,6 +3,5 @@ emacs -Q --batch \
       --eval "(require 'org)" \
       --eval "(require 'ob-shell)" \
       --eval "(setq org-confirm-babel-evaluate nil)" \
-      --eval "(org-babel-tangle-file \"~/.emacs.d/README.org\" \"~/.emacs.d/README.el\")" \
       --eval "(org-babel-tangle-file \"~/.emacs.d/dotfiles/fish.org\")" \
       --eval "(org-babel-tangle-file \"~/.emacs.d/dotfiles/i3.org\")"
