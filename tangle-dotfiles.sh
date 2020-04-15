d4b1cca936e8c23d3b7e729ceb501d2e4fa5fec4
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-04-15 09:25:27 -0700

    remove dotfiles

--- tangle-dotfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-emacs -Q --batch \
-      --eval "(require 'org)" \
-      --eval "(require 'ob-shell)" \
-      --eval "(setq org-confirm-babel-evaluate nil)" \
-      --eval "(org-babel-tangle-file \"~/.emacs.d/dotfiles/fish.org\")" \
-      --eval "(org-babel-tangle-file \"~/.emacs.d/dotfiles/i3.org\")"
